bb10c2bee137ea2b4bea30852c3fddbe82a0f770
Yehor Popovych <y.popovych@crossroadlabs.xyz>
2016-03-02 16:01:11 +0200

    Linux config and tests

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+@testable import UVtest
+
+XCTMain([
+	LoopTests(),
+	UVTests(),
+])
--- Tests/UV/UVTests.swift
+++ Tests/UV/UVTests.swift
@@ -20,16 +20,6 @@ import CUV
 
 class UVTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
         let loop = try? Loop()
         
@@ -46,3 +36,14 @@ class UVTests: XCTestCase {
     }
     
 }
+
+#if os(Linux)
+extension UVTests : XCTestCaseProvider {
+	var allTests : [(String, () throws -> Void)] {
+		return [
+			("testExample", testExample),
+			("testPerformanceExample", testPerformanceExample),
+		]
+	}
+}
+#endif
